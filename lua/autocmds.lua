35bb5478f03cb5826e4d9a585474e98ef5af36dc
Zdravko Kosanovic <zdravkokg@mail.ru>
2024-12-08 06:05:15 +0100

    Auto open nvim-tree when opening a directory

---
+++ lua/autocmds.lua
@@ -0,0 +1,21 @@
+require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- open nvim-tree when opening a directory
+local function open_nvim_tree(data)
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+autocmd({ "VimEnter" }, { callback = open_nvim_tree })
